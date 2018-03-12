b9dda5bbc25763ebbe19c7795f3a7c1b060155e0
dingjianglei77 <2215189961@qq.com>
2018-03-12 21:25:35 +0800

    test

--- src/test/java/UserTest.java
+++ src/test/java/UserTest.java
@@ -1,6 +1,6 @@
 public class UserTest {
     public  void test(){
-        System.out.println("djl-test");
-        System.out.println("djl-test2");//test
+        System.out.println("djl-test");//33
+        System.out.println("djl-test2");//test222
     }
 }
